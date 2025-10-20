2983ab47a5779565a0fae8ec67e6839610991c81
smitsky <carshine56@yahoo.com>
2025-10-20 16:46:55 -0400

    Update tutorial3.py

--- tutorial3.py
+++ tutorial3.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = "wxyz"
 
 # PERFECT DYNAMIC DB URL
-db_url = os.getenv('DATABASE_URL', 'sqlite:///users.sqlite3')
+db_url = os.getenv('DATABASE_URL', 'sqlite:///instance/users.sqlite3')
 if db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
