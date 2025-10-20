edd3000fc32050cd5566d152f7c47191b75519b5
smitsky <carshine56@yahoo.com>
2025-10-20 16:33:50 -0400

    Create tutorial3.py

---
+++ tutorial3.py
@@ -0,0 +1,102 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.secret_key = "wxyz"
+
+# PERFECT DYNAMIC DB URL
+db_url = os.getenv('DATABASE_URL', 'sqlite:///users.sqlite3')
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(days=5)
+
+db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
+class users(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/view')
+def view():
+    return render_template('view.html', values=users.query.all())
+
+@app.route('/delete/<int:user_id>')
+def delete(user_id):
+    user_to_delete = users.query.get(user_id)
+    if user_to_delete:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User deleted Successfully", "success")
+    else:
+        flash("User not found", "error")
+    return redirect(url_for('view'))
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
+        flash("Login Successful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route('/user', methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Your email has been saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route('/logout')
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("login"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
