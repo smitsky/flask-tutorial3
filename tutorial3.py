6ae274222e47f43420bef3ba138ea65cb2adaf86
smitsky <carshine56@yahoo.com>
2025-10-20 17:48:47 -0400

    Update tutorial3.py

--- tutorial3.py
+++ tutorial3.py
@@ -1,3 +1,7 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.secret_key = "wxyz"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
